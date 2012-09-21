5581910bbfbceaac3f03447e094abfceef5b73fe
Sivaprakash Ragavan <sivaprakash.ragavan@gmail.com>
2012-09-21 16:29:27 +0530

    using plain java mongodb driver

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,46 +1,34 @@
 package controllers;
 
-import models.Group;
-import play.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
+import plugins.MongoPlugin;
 
-import com.google.code.morphia.Datastore;
-import com.google.code.morphia.Morphia;
-import com.google.code.morphia.mapping.Mapper;
-import com.google.code.morphia.query.UpdateOperations;
-import com.mongodb.DB;
-import com.mongodb.Mongo;
-import com.mongodb.MongoURI;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
 
 public class Application extends Controller {
 
 	public static Result index() throws Exception {
-		Morphia morphia = new Morphia();
-		Mongo mongo = new Mongo("ds037907-a.mongolab.com", 37907);
-		DB db = mongo.getDB("heroku_app7757420");
-		//System.out.println(db.authenticate("heroku_app7757420", "r82c6febvl4abqgn6bphg7lr1q".toCharArray()));
-		Datastore ds = morphia.createDatastore(mongo, "heroku_app7757420", "heroku_app7757420", "r82c6febvl4abqgn6bphg7lr1q".toCharArray());
-		ds.ensureIndexes();
-		ds.ensureCaps();
 
-		Group me = new Group();
-		me.groupName = "test group";
-		ds.save(me);
-		Group e3 = ds.find(Group.class).get();
-		UpdateOperations<Group> ops = ds.createUpdateOperations(Group.class)
-				.set("groupName", "hello world");
-		// UpdateOperations<Group> ops =
-		// ds.createUpdateOperations(Group.class).unset("name");
-		ds.update(
-				ds.createQuery(Group.class).field(Mapper.ID_KEY).equal(e3.id),
-				ops);
-		String result = "";
-		for (Group me2 : ds.find(Group.class)) {
-			System.out.println(me2.groupName);
-			result = me2.groupName;
-		}
-		return ok(result);
+		DBCollection coll = MongoPlugin.db.getCollection("testCollection");
+
+		BasicDBObject doc = new BasicDBObject();
+
+		doc.put("name", "MongoDB");
+		doc.put("type", "database");
+		doc.put("count", 1);
+
+		BasicDBObject info = new BasicDBObject();
+
+		info.put("x", 203);
+		info.put("y", 102);
+
+		doc.put("info", info);
+
+		coll.insert(doc);
+
+		return ok("True");
 	}
 
 	public static Result addUser(final String url) {
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table article (
+  id                        varchar(40) not null,
+  url                       varchar(255),
+  title                     varchar(255),
+  text                      varchar(255),
+  json_res                  varchar(255),
+  total_length              integer,
+  download_length           integer,
+  constraint pk_article primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists article;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
